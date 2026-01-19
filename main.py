e7e505d278384a3692599ef087daece3065cd56f
yuvaashree-k <yuvaashreek@gmail.com>
2026-01-19 19:02:42 +0530

    Added canvas and embedded turtle using Rawturtle

--- main.py
+++ main.py
@@ -5,4 +5,13 @@ root =tk.Tk() #creating the main window
 root.title("L-System Fractal Architect")
 root.geometry("1000x700")
 root.resizable(False,False) #prevent resizing
+canvas = tk.Canvas(root, width=700, height=700, bg="white") #area where graphics can be drawn
+canvas.pack(side=tk.LEFT) #places canvas on the left side
+screen = turtle.TurtleScreen(canvas) #uses this tkinter canvas as the screen
+pen = turtle.RawTurtle(screen) #create the raw turtle object
+#initial turtle settings
+pen.speed(0) #fastest drawing
+pen.hideturtle() #hides arrow for clean visuals
+   
 root.mainloop() #keeps the window open 
+
