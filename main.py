16cf0ef17d59573b28500c14cca22d92f2900a03
yuvaashree-k <yuvaashreek@gmail.com>
2026-01-19 18:45:35 +0530

    Created main Tkinter window

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 # L-System Fractal Architect
+import tkinter as tk
+import turtle
+root =tk.Tk() #creating the main window
+root.title("L-System Fractal Architect")
+root.geometry("1000x700")
+root.resizable(False,False) #prevent resizing
+root.mainloop() #keeps the window open 
